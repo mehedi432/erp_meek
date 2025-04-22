ad38220281221e2db1441ebb028401e145f0c444
mehedi432 <aamehedi93@gmail.com>
2025-04-22 20:41:10 +0600

    Initial Commit

---
+++ meek/sample_development/doctype/sample_request_v1/sample_request_v1.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Mehedi Abdullah and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Sample Request V1", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ meek/sample_section/doctype/sample_request_old/test_sample_request_old.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Mehedi Abdullah and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSampleRequestOld(FrappeTestCase):
+	pass
